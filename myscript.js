3ecf2b485f7e561c82705f48462955bacfd1fba2
Maciej S <115628283+MACKOzKLANU@users.noreply.github.com>
2023-05-30 22:57:38 +0200

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    document.body.style.backgroundColor = "black";
+    
+
+    (document.getElementById("dot11")).style.backgroundColor = "white";
+    (document.getElementById("dot22")).style.backgroundColor = "white";
+    (document.getElementById("dot33")).style.backgroundColor = "white";
+    (document.getElementById("boxx")).style.backgroundColor = "gray";
+  
+
+
+
+}
+function myFunction1() {
+    
+    document.body.style.backgroundColor = "white";
+
+    (document.getElementById("dot11")).style.backgroundColor = "black";
+    (document.getElementById("dot22")).style.backgroundColor = "black";
+    (document.getElementById("dot33")).style.backgroundColor = "black";
+    (document.getElementById("boxx")).style.backgroundColor = "darkgray";
+  }
+  
